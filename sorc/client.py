391b2854e87960ee3f6fe97a4ffca5a51d835a41
kyle merfeld <kmerfeld1@gmail.com>
2016-10-01 10:38:53 -0500

    updates

--- sorc/client.py
+++ sorc/client.py
@@ -28,7 +28,7 @@ def initialResponse():
                 {"CharacterName": "Heal_Bitch",
                  "ClassId": "Paladin"},
                 {"CharacterName": "TANK",
-                 "ClassId": "Paladin"},
+                 "ClassId": "Archer"},
             ]}
 # ---------------------------------------------------------------------
 
@@ -52,28 +52,28 @@ def processTurn(serverResponse):
                 character.serialize(characterJson)
                 enemyteam.append(character)
 # ------------------ You shouldn't change above but you can ---------------
-       
+      
 
     #Value of characters
     def get_priority( char ):
         
         if char.classId == "Archer":
-            priority = 4 * (1001 - char.attributes.health)
-        elif char.classId == "Assasin":
+            priority = 10 * (1001 - char.attributes.health)   
+        elif char.classId == "Assassin":
             priority = 9 * (801 - char.attributes.health)
         elif char.classId == "Druid":
             priority = 8 * (1001 - char.attributes.health)
         elif char.classId == "Enchanter":
-            priority = 10 * (1001 - char.attributes.health)
+            priority = 4 * (1001 - char.attributes.health)
         elif char.classId == "Paladin":
-            priority = 7 * (1101 - char.attributes.health)
+            priority = 2 * (1 + (1100 - char.attributes.health))
         elif char.classId == "Sorcerer":
             priority = 6 * (801 - char.attributes.health)
         elif char.classId == "Warrior":
             priority = 3 * (1201 - char.attributes.health)
         elif char.classId == "Wizard":
             priority = 5 * (1001 - char.attributes.health)
-        print("Priority is " + str(priority))
+
         return priority
     # Choose a target
     target = None
@@ -100,63 +100,145 @@ def processTurn(serverResponse):
         for character in myteam:
 
             # If I am in range, either move towards target
+            cast = True
             if character.in_range_of(target, gameMap):
+                
 
-                # Am I already trying to cast something?
+                                # Am I already trying to cast something?
                 if character.casting is None:
+
                     cast = False
-                        
+
+                ## use burst if needed
+                #check if cc'ed
+                
+                    if character.attributes.get_attribute("Stunnded") or character.attributes.get_attribute("Silenced") or character.attributes.get_attribute("Rooted"):
+                        print( "unit is cced")
+                        if character.abilities.items()[0][0] == 0:
+                            #use breakout
+                            print("ATTEMPTING TO REMOVE CC")
+
+                            actions.append({
+                                                    "Action": "Cast",
+                                                    "CharacterId": character.id,
+                                                    "TargetId": character.id,
+                                                    "AbilityId": 0
+                                                })
+                            cast = True
+                            break
+
+
+            
                     if character.classId == "Paladin":
                         
-                        #print(character.can_use_ability(3))
-                        #if character.can_use_ability(3):
-                        if 1 == 1:
+                        if character.abilities.items()[2][1] == 0:
                             a = 0
-                            s = False
-                            for unit in myteam:
-                                if unit.attributes.health < 600 and unit.attributes.health > 0:
-                                    s = True
-                            if s:       
+                            #check if opponent is already stunned
+                            if not target.attributes.get_attribute("Stunned"):
+                                print("CASTING STUN THE GODS")
                                 #heal
-                                to_heal = 0
+                                to_stun = 0
                                 thing = None
-                                for unit in myteam:
+                                #check for unit most in need of healing
+                                for unit in enemyteam:
                                     v = get_priority(unit)
-                                    if v > to_heal:
+                                    if v > to_stun:
                                         thing = unit
+                                        to_stun = v
                                         
                                 actions.append({
                                             "Action": "Cast",
                                             "CharacterId": character.id,
                                             "TargetId": thing.id,
-                                            "AbilityId": 3
+                                            "AbilityId": 14
                                         })
                                 cast = True
                                 break
+                            else:
 
-                        if character.can_use_ability(4):
-                            #stun
-                            to_stun = 0
-                            thing = None
-                            for unit in enemyteam:
-                                v = get_priority(unit)
-                                if v > to_stun:
-                                    thing = unit
+                                print("Already stunned")
 
-                            print("Paladin is going to stun " + to_stun.classId + "Its health is " + str(to_stun.attributes.health))
-                            actions.append({
+                        #heal
+                        if character.abilities.items()[1][1] == 0:
+                            a = 0
+                            s = False
+                            for unit in myteam:
+                                if unit.attributes.health < 799 and unit.attributes.health > 0:
+                                    s = True
+                            if s:       
+
+                                #heal
+                                to_heal = 0
+                                thing = None
+                                #check for unit most in need of healing
+                                for unit in myteam:
+                                    v = get_priority(unit)
+                                    if v > to_heal:
+                                        to_heal = v
+                                        thing = unit
+                                if to_heal > 0:
+
+                                    print("healing " + thing.name + " its prio is " + str(to_heal))                                     
+                                    actions.append({
+                                                "Action": "Cast",
+                                                "CharacterId": character.id,
+                                                "TargetId": thing.id,
+                                                "AbilityId": 3
+                                            })
+                                    cast = True
+                                    break
+
+                        cast = False
+                            
+                    if character.classId == "Archer":
+                        
+                        if character.abilities.items()[1][1] == 0:
+                            a = 0
+                            s = True
+                            if s:       
+                                print("Going to Armor debuff")
+                                #heal
+                                to_stun = 0
+                                thing = None
+                                #check for unit most in need of lessarmor
+                                for unit in enemyteam:
+                                    v = get_priority(unit)
+                                    if v > to_stun:
+                                        thing = unit
+                                        to_stun = v
+                                        
+                                actions.append({
+                                            "Action": "Cast",
+                                            "CharacterId": character.id,
+                                            "TargetId": thing.id,
+                                            "AbilityId": 2
+                                        })
+                                cast = True
+                                break
+                    #handle character not writen
+                    else:
+                        if character.casting is None:
+                            cast = False
+                            for abilityId, cooldown in character.abilities.items():
+                                # Do I have an ability not on cooldown
+                                if cooldown == 0:
+                                    # If I can, then cast it
+                                    ability = game_consts.abilitiesList[int(abilityId)]
+                                    # Get ability
+                                    actions.append({
                                         "Action": "Cast",
                                         "CharacterId": character.id,
                                         # Am I buffing or debuffing? If buffing, target myself
-                                        "TargetId": thing.id,
-                                        "AbilityId": 14
+                                        "TargetId": target.id if ability["StatChanges"][0]["Change"] < 0 else character.id,
+                                        "AbilityId": int(abilityId)
                                     })
-                            break
-
+                                    cast = True
+                                    break
 
                 ## Not Class Specific                    
                 # Was I able to cast something? Either wise attack
-                if 1 == 1:
+                if cast == True:
+                    
                     actions.append({
                         "Action": "Attack",
                         "CharacterId": character.id,
